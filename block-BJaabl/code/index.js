725e6b28f49cda0f5f10f87b4cdd53b275b800a7
tinkalguli <tinkalmunu@gmail.com>
2020-08-18 02:03:19 +0530

    Complete writing your first function block-4

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,10 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+function sayHello(name) {
+  alert(`Hello ${name}!`);
+}
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +14,10 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -20,6 +28,14 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+
+function addTwoNumbers(firstNum, secondNum) {
+  if (isNaN(firstNum) || isNaN(secondNum)) {
+    alert(`Enter Valid Input`);
+  } else {
+    return firstNum + secondNum;
+  }
+}
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,6 +49,22 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc(numA, numB, operation) {
+ if(isNaN(numA) || isNaN(numB)) {
+    alert(`Enter Valid Input`);
+ } else {
+   switch(operation) {
+     case "add":
+       return numA + numB;
+       break;
+     case "sub":
+       return numA - numB;
+       break;
+     case "mul":
+       return numA * numB;
+   }
+ }
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +74,19 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear(year) {
+  return year % 4 ? false : true;
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+
+function getFactorial(number) {
+  let factorial = 1;
+  while (number >= 1) {
+    factorial *= number;
+    number--;
+  }
+  return factorial;
+}
